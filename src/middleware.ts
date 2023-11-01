0ff148060c4465999c5cca3ae355ee14ba467f67
donnailyukh <donnailyukh@gmail.com>
2023-11-01 14:57:06 -0400

    api typo

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const password = request.nextUrl.searchParams.get('password');
 
-  if (password !== process.env.API_SECRET) {
+  if (password !== process.env.API_SECRET_KEY) {
     return NextResponse.json({ message: 'Wrong password' }, { status: 403 });
   }
 
